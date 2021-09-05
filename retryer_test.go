b9a0c541c540383f0f7bfe59026ef76689e25cf1
Atsushi Watanabe <atsushi.w@ieee.org>
2021-09-05 13:46:58 +0900

    Fix ExponentialBackoffRetryer (#4)

---
+++ retryer_test.go
@@ -0,0 +1,141 @@
+package s3iot
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestNoRetryer(t *testing.T) {
+	f := &NoRetryerFactory{}
+	r := f.New()
+	if cont := r.OnFail(0, nil); cont {
+		t.Error("Unexpected retry")
+	}
+	r.OnSuccess(0)
+}
+
+func TestExponentialBackoffRetryer(t *testing.T) {
+	f := &ExponentialBackoffRetryerFactory{
+		WaitBase: 50 * time.Millisecond,
+		WaitMax:  250 * time.Millisecond,
+		RetryMax: 4,
+	}
+	r := f.New()
+	errTemp := &errTemp{}
+
+	ts := time.Now()
+	for i := 0; i < 4; i++ {
+		if cont := r.OnFail(0, errTemp); !cont {
+			t.Error("Unexpected failure before reaching RetryMax")
+		}
+	}
+	te := time.Now()
+	// 50 + 100 + 200 + 250 = 600ms
+	expectedWait := 600 * time.Millisecond
+	tolerance := 50 * time.Millisecond
+	if diff := te.Sub(ts) - expectedWait; diff < -tolerance || tolerance < diff {
+		t.Errorf("Expected wait: %v, actual: %v", te.Sub(ts), expectedWait)
+	}
+
+	if cont := r.OnFail(1, errTemp); !cont {
+		t.Error("Unexpected failure on different id")
+	}
+
+	if cont := r.OnFail(0, errTemp); cont {
+		t.Error("Unexpected retry after reaching RetryMax")
+	}
+	r.OnSuccess(0)
+
+	if cont := r.OnFail(0, errTemp); !cont {
+		t.Error("Unexpected failure after resetting failure")
+	}
+
+	if cont := r.OnFail(2, errors.New("test")); cont {
+		t.Error("Non-temporary error must cause immediate stop")
+	}
+}
+
+func TestWithRetry(t *testing.T) {
+	f := &ExponentialBackoffRetryerFactory{
+		WaitBase: 1 * time.Millisecond,
+		RetryMax: 1,
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		r := f.New()
+		var i int
+		err := withRetry(context.TODO(), 0, r, func() error {
+			defer func() {
+				i++
+			}()
+			switch i {
+			case 0:
+				return nil
+			default:
+				t.Fatal("Must not reach here")
+				return nil
+			}
+		})
+		if err != nil {
+			t.Error("Unexpected failure")
+		}
+		if i != 1 {
+			t.Errorf("Expected retry count: 1, actual: %d", i)
+		}
+	})
+	t.Run("SuccessAfterRetry", func(t *testing.T) {
+		r := f.New()
+		var i int
+		err := withRetry(context.TODO(), 0, r, func() error {
+			defer func() {
+				i++
+			}()
+			switch i {
+			case 0:
+				return &errTemp{}
+			case 1:
+				return nil
+			default:
+				t.Fatal("Must not reach here")
+				return nil
+			}
+		})
+		if err != nil {
+			t.Error("Unexpected failure")
+		}
+		if i != 2 {
+			t.Errorf("Expected retry count: 2, actual: %d", i)
+		}
+	})
+	t.Run("Failure", func(t *testing.T) {
+		r := f.New()
+		var i int
+		err := withRetry(context.TODO(), 0, r, func() error {
+			defer func() {
+				i++
+			}()
+			switch i {
+			case 0, 1:
+				return &errTemp{}
+			default:
+				t.Fatal("Must not reach here")
+				return nil
+			}
+		})
+		if err == nil {
+			t.Error("Expected failure, but succeeded")
+		}
+		if i != 2 {
+			t.Errorf("Expected retry count: 2, actual: %d", i)
+		}
+	})
+
+}
+
+type errTemp struct{}
+
+func (errTemp) Temporary() bool { return true }
+func (errTemp) Timeout() bool   { return true }
+func (errTemp) Error() string   { return "error" }
