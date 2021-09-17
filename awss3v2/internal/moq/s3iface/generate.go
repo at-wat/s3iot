7ed6bace18e75237b15b94a6eb3925cbcef6800e
Atsushi Watanabe <atsushi.w@ieee.org>
2021-09-18 00:10:34 +0900

    Apply golint to all dirs (#41)

--- awss3v2/internal/moq/s3iface/generate.go
+++ awss3v2/internal/moq/s3iface/generate.go
@@ -8,10 +8,12 @@ import (
 
 //go:generate go run github.com/matryer/moq -out s3iface.go . S3API:MockS3API HTTPClient:MockHTTPClient
 
+// S3API for generating mock.
 type S3API interface {
 	awss3v2.S3API
 }
 
+// HTTPClient for generating mock.
 type HTTPClient interface {
 	s3.HTTPClient
 }
