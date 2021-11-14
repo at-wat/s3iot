72ced2d3cdd06c15d2e5aaa6b0c5fd9458d36938
Atsushi Watanabe <atsushi.w@ieee.org>
2021-11-14 09:55:44 +0900

    awss3v1: retry on connection reset (#71)

--- awss3v2/errclassifier_test.go
+++ awss3v2/errclassifier_test.go
@@ -15,15 +15,67 @@
 package awss3v2
 
 import (
+	"context"
 	"errors"
+	"net"
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/smithy-go"
 	"github.com/aws/smithy-go/transport/http"
 )
 
 func TestErrorClassifier(t *testing.T) {
+	// Generate actual network errors
+	config := func(endpoint string) aws.Config {
+		cfg, err := config.LoadDefaultConfig(context.TODO(),
+			config.WithCredentialsProvider(
+				credentials.NewStaticCredentialsProvider("", "", ""),
+			),
+			config.WithEndpointResolver(aws.EndpointResolverFunc(
+				func(service, region string) (aws.Endpoint, error) {
+					return aws.Endpoint{URL: "http://" + endpoint}, nil
+				},
+			)),
+			config.WithRetryer(func() aws.Retryer {
+				return aws.NopRetryer{}
+			}),
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return cfg
+	}
+	s3Config := func(o *s3.Options) {
+		o.UsePathStyle = true
+	}
+	_, errConnRefused := s3.NewFromConfig(
+		config("localhost:0"), s3Config,
+	).PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	})
+	ln, err := net.Listen("tcp4", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		conn, _ := ln.Accept()
+		conn.Read(make([]byte, 1))
+		conn.Close()
+		ln.Close()
+	}()
+	_, errConnReset := s3.NewFromConfig(
+		config(ln.Addr().String()), s3Config,
+	).PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	})
+
 	testCases := map[string]struct {
 		err       error
 		retryable bool
@@ -69,11 +121,20 @@ func TestErrorClassifier(t *testing.T) {
 			waitParam: time.Minute,
 			wait:      time.Minute,
 		},
+		"AWSConnRefused": {
+			err:       errConnRefused,
+			retryable: true,
+		},
+		"AWSConnReset": {
+			err:       errConnReset,
+			retryable: true,
+		},
 	}
 
 	for name, tt := range testCases {
 		tt := tt
 		t.Run(name, func(t *testing.T) {
+			t.Log(name, tt.err)
 			ec := &ErrorClassifier{
 				ThrottleWait: tt.waitParam,
 			}
