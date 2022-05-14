f59c0a079dd43cdd1aa12001d26fe69936344c1e
Atsushi Watanabe <atsushi.w@ieee.org>
2022-05-14 21:41:41 +0900

    Add godoc

--- awss3v1/internal/moq/s3iface/iface/iface.go
+++ awss3v1/internal/moq/s3iface/iface/iface.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
 
+// S3API for generating mock.
 type S3API interface {
 	s3iface.S3API
 }
--- awss3v2/internal/moq/s3iface/iface/iface.go
+++ awss3v2/internal/moq/s3iface/iface/iface.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+// HTTPClient for generating mock.
 type HTTPClient interface {
 	s3.HTTPClient
 }
