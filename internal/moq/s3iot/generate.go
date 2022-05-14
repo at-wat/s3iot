a58f270c2b01568374ca3b990fbd59d03da5122b
Atsushi Watanabe <atsushi.w@ieee.org>
2022-05-14 21:43:06 +0900

    Fix typo

--- internal/moq/s3iot/generate.go
+++ internal/moq/s3iot/generate.go
@@ -1,3 +1,3 @@
 package mock_s3iot
 
-//go:generate go run github.com/matryer/moq -pkg mocks_3iot -out generated.go ../../.. S3API:MockS3API ReadInterceptorFactory:MockReadInterceptorFactory ReadInterceptor:MockReadInterceptor
+//go:generate go run github.com/matryer/moq -pkg mock_s3iot -out generated.go ../../.. S3API:MockS3API ReadInterceptorFactory:MockReadInterceptorFactory ReadInterceptor:MockReadInterceptor
