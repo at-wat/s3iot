7bcaab6dd5e88b33c1dfb53945f6d6c6a4f8d11f
Atsushi Watanabe <atsushi.w@ieee.org>
2022-05-16 01:02:49 +0900

    Add godoc

--- s3iotiface/s3iotiface.go
+++ s3iotiface/s3iotiface.go
@@ -7,10 +7,12 @@ import (
 	"github.com/at-wat/s3iot"
 )
 
+// Uploader interface of s3iot.
 type Uploader interface {
 	Upload(ctx context.Context, input *s3iot.UploadInput) (s3iot.UploadContext, error)
 }
 
+// Downloader interface of s3iot.
 type Downloader interface {
 	Download(ctx context.Context, w io.WriterAt, input *s3iot.DownloadInput) (s3iot.DownloadContext, error)
 }
