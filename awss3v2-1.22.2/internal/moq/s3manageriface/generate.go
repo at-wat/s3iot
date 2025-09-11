e3f7a26296976b63f194bf4c2eec075a0c6218c6
Atsushi Watanabe <atsushi.w@ieee.org>
2025-09-11 10:50:50 +0900

    Use system installed moq to avoid Go version update due to moq

--- awss3v2-1.22.2/internal/moq/s3manageriface/generate.go
+++ awss3v2-1.22.2/internal/moq/s3manageriface/generate.go
@@ -1,3 +1,10 @@
 package mock_s3manageriface // revive:disable-line:var-naming
 
-//go:generate go run github.com/matryer/moq -pkg mock_s3manageriface -out generated.go ../../../s3manageriface Uploader:MockUploader Downloader:MockDownloader
+// Since moq declares only the latest Go version is supported,
+// moq version can't be controlled under our go.mod.
+//
+// So, moq must be installed by:
+//   cd s3iot/tools
+//   go install github.com/matryer/moq
+
+//go:generate moq -pkg mock_s3manageriface -out generated.go ../../../s3manageriface Uploader:MockUploader Downloader:MockDownloader
